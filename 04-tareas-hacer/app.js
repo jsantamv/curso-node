18cc1358e8f7fae95bff171fe115c3d475014b8b
Juan Carlos Santamaria <jsantamaria@namutek.com>
2021-03-24 21:09:23 -0600

    actualizar los datos

--- 04-tareas-hacer/app.js
+++ 04-tareas-hacer/app.js
@@ -1,5 +1,5 @@
 require('colors')
-const { inquirerMenu } = require('./helpers/inquirer')
+const { inquirerMenu, pausa } = require('./helpers/inquirer')
 
 const main = async () => {
 
@@ -7,8 +7,9 @@ const main = async () => {
 
     do {
         opt = await inquirerMenu()
-        console.log({opt})
-        //await pausa()
+        console.log({ opt })
+        await pausa()
+
     } while (opt !== '0')
 }
 
--- 04-tareas-hacer/helpers/inquirer.js
+++ 04-tareas-hacer/helpers/inquirer.js
@@ -6,10 +6,42 @@ const preguntas = [
     {
         type: 'list',
         name: 'option',
-        message:'Que desea hacer?',
-        choices: ['opt1','opt2','opt3','opt4']
+        message: 'Que desea hacer?',
+        choices: [
+            {
+                value: '1',
+                name: '1. Crear Tarea'
+            },
+            {
+                value: '2',
+                name: '2. Listar Tareas'
+            },
+            {
+                value: '3',
+                name: '3. Listar tareas completadas'
+            },
+            {
+                value: '4',
+                name: '4. Listar tareas pendientes'
+            },
+            {
+                value: '5',
+                name: '5. Completar tarea(s)'
+            },
+            {
+                value: '6',
+                name: '6. Crear Tarea'
+            },
+            {
+                value: '7',
+                name: '7. Borrar Tarea'
+            },
+            {
+                value: '0',
+                name: 'O. SALIR'
+            }
+        ]
     }
-
 ]
 
 const inquirerMenu = async () => {
@@ -18,12 +50,25 @@ const inquirerMenu = async () => {
     console.log('   SELECIONE UNA OPCION '.yellow)
     console.log('====================================\n'.green)
 
-    const opt = await inquirer.prompt(preguntas)
+    const { opt } = await inquirer.prompt(preguntas)
 
     return opt;
+}
 
+const pausa = async () => {
+
+    const quetion = [
+        {
+            type: 'input',
+            name: 'enter',
+            message: `Presione ${'enter'.green} para continuar`
+        }
+    ]
+    console.log('\n')
+    await inquirer.prompt(quetion)
 }
 
 module.exports = {
     inquirerMenu,
+    pausa
 }
